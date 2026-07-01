d1efda5c4ad5e62a2109774d965f25b6ef8e7931
Mouloud78 <mouloudouldali78@gmail.com>
2026-07-01 09:46:47 -0400

    création du context EF

--- DCI-Infrastructure/Data/ApplicationDbContext.cs
+++ DCI-Infrastructure/Data/ApplicationDbContext.cs
@@ -1,8 +1,43 @@
-﻿
+﻿using DCI_Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DCI_Infrastructure.Data
 {
-    internal class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Patient> Patients => Set<Patient>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Patient>(entity =>
+            {
+                entity.Property(p => p.FirstName)
+                      .HasMaxLength(100)
+                      .IsRequired();
+
+                entity.Property(p => p.LastName)
+                      .HasMaxLength(100)
+                      .IsRequired();
+
+                entity.Property(p => p.Phone)
+                      .HasMaxLength(20);
+
+                entity.Property(p => p.Email)
+                      .HasMaxLength(150);
+
+                entity.Property(p => p.Address)
+                      .HasMaxLength(250);
+
+                entity.Property(p => p.BloodGroup)
+                      .HasMaxLength(5);
+            });
+        }
     }
 }
+
